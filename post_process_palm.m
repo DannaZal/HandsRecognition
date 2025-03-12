5dcc1933af358918ccc09b304058f86ff22c0b52
DannaZal <danna.zaldumbide@epn.edu.ec>
2025-03-12 12:48:07 -0500

    Limite para detectar manos = 3

--- post_process_palm.m
+++ post_process_palm.m
@@ -1,5 +1,7 @@
 function hands = post_process(image, boxes)
-    score_threshold=0.8;
+    score_threshold = 0.8;
+    max_hands = 3;  % Máximo de manos a detectar
+    
     % Obtener dimensiones de la imagen
     image_height = size(image, 1);
     image_width = size(image, 2);
@@ -13,9 +15,15 @@ function hands = post_process(image, boxes)
     boxes = boxes(keep, :);
     
     hands = [];
-
+    
+    % Limitar el número de manos detectadas
+    num_hands_detected = 0;
+    
     for i = 1:size(boxes, 1)
-
+        if num_hands_detected >= max_hands
+            break; % Detener el loop si ya se detectaron 2 manos
+        end
+        
         box_x = boxes(i, 2);
         box_y = boxes(i, 3);
         box_size = boxes(i, 4);
@@ -30,21 +38,22 @@ function hands = post_process(image, boxes)
             sqn_rr_size = 2.0 * box_size;
             rotation = 0.5 * pi - atan2(-kp02_y, kp02_x);
             
-            % Normalizar rotación si es necesario
+            % Normalizar rotación
             rotation = normalize_radians(rotation);  
             
             sqn_rr_center_x = box_x + 0.5 * box_size * sin(rotation);
             sqn_rr_center_y = box_y - 0.5 * box_size * cos(rotation);
-            %sqn_rr_center_y = (sqn_rr_center_y * square_standard_size - square_padding_half_size) / image_height;
             
             % Agregar resultado a la lista
             hands = [hands; [sqn_rr_size, rotation, sqn_rr_center_x, sqn_rr_center_y]];
+            
+            % Incrementar el contador de manos detectadas
+            num_hands_detected = num_hands_detected + 1;
         end
     end
 end
 
-
 function normalized_angle = normalize_radians(angle)
     % Normaliza el ángulo en radianes al rango [-pi, pi]
-    normalized_angle = angle - 2 * pi * floor((angle + pi) / (2 * pi));
+    normalized_angle = mod(angle + pi, 2 * pi) - pi;
 end
